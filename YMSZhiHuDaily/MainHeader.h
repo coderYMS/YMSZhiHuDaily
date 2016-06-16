91e7ca4fe59034d6c92ae2171209a137559db22a
coderYMS <yu6mengshi@sina.com>
2016-06-16 09:47:42 +0800

    first ci

---
+++ YMSZhiHuDaily/MainHeader.h
@@ -0,0 +1,20 @@
+//
+//  MainHeader.h
+//  YMSZhiHuDaily
+//
+//  Created by mac on 16/6/16.
+//  Copyright © 2016年 yms. All rights reserved.
+//
+
+#ifndef MainHeader_h
+#define MainHeader_h
+
+#define kScreenBounds [UIScreen mainScreen].bounds
+#define kScreenWidth  [UIScreen mainScreen].bounds.size.width
+#define kScreenHeight [UIScreen mainScreen].bounds.size.height
+
+#define kThemeColor [UIColor colorWithRed:60.f/255.f green:198.f/255.f blue:253.f/255.f alpha:1.f];
+
+
+
+#endif /* MainHeader_h */
